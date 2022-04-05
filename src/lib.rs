889876d6feba63e93314118d35a7ebe16afc662c
Juan <jaguirreb@fi.uba.ar>
2022-04-05 16:21:20 -0300

    Terminar documentacion

--- src/lib.rs
+++ src/lib.rs
@@ -4,26 +4,43 @@ pub mod files;
 pub mod grid;
 pub mod lcs;
 
+/// Se representa la operacion de diff
 pub struct Diff<'a> {
+    /// La grilla resultante de aplicar el algoritmos de LCS.
     grid: Grid,
-    x: &'a [String],
-    y: &'a [String],
+    /// La secuencia original.
+    original: &'a [String],
+    /// La secuencia modificada.
+    modified: &'a [String],
 }
 
 impl<'a> Diff<'a> {
-    pub fn new(x: &'a [String], y: &'a [String]) -> Self {
+    /// Crea un nuevo Diff, a partir de las dos secuencias de String.
+    /// # Arguments
+    /// * `original` y `modified` - Slice de strings entre
+    /// los cuales se realiza el diff.
+    pub fn new(original: &'a [String], modified: &'a [String]) -> Self {
         Diff {
-            grid: lcs::lcs(x, y),
-            x,
-            y,
+            grid: lcs::lcs(original, modified),
+            original,
+            modified,
         }
     }
 
+    /// Imprime por stdout el diff entre las dos secuencias.
     pub fn print_diff(&self) {
-        print_diff(&self.grid, self.x, self.y, self.x.len(), self.y.len());
+        print_diff(
+            &self.grid,
+            self.original,
+            self.modified,
+            self.original.len(),
+            self.modified.len(),
+        );
     }
 }
 
+/// Funcion auxiliar que imprime por stdout el diff entre las dos
+/// secuencias de manera recursiva.
 fn print_diff(grid: &Grid, x: &[String], y: &[String], i: usize, j: usize) {
     if i > 0 && j > 0 && x[i - 1] == y[j - 1] {
         print_diff(grid, x, y, i - 1, j - 1);
